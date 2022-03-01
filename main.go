9e35a662e9046a7b8e5f58f02c2b0d6f627d7047
HOPOLOK <vadim.truhanov@inbox.lv>
2022-03-01 22:43:33 +0000

    uint64 fix

--- main.go
+++ main.go
@@ -3,21 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var N int
+	var N uint64
 	fmt.Scan(&N)
-	var res int
-	for i := 0; i <= 9*N; i++ {
+	var res uint64
+	for i := uint64(0); i <= 9*N; i++ {
 		temp := T(N, i)
 		res += temp * temp
 	}
 	fmt.Print(res)
 }
 
-func T(n, s int) int {
+func T(n, s uint64) uint64 {
 	if n == 1 {
 		return 1
 	}
-	var res int
+	var res uint64
 	for i := s - 9; i <= s; i++ {
 		if i < 0 || i > 9*(n-1) {
 			continue
