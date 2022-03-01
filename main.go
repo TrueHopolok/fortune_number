b5410cb23ff07c7c54d2c5a81d56c522bfc9af55
HOPOLOK <vadim.truhanov@inbox.lv>
2022-03-01 22:15:03 +0000

    Completed partly. Can't run with larger than 8. Can't save into files

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var N int
+	fmt.Scan(&N)
+	var res int
+	for i := 0; i <= 9*N; i++ {
+		temp := T(N, i)
+		res += temp * temp
+	}
+	fmt.Print(res)
+}
+
+func T(n, s int) int {
+	if n == 1 {
+		return 1
+	}
+	var res int
+	for i := s - 9; i <= s; i++ {
+		if i < 0 || i > 9*(n-1) {
+			continue
+		}
+		res += T(n-1, i)
+	}
+	return res
+}
